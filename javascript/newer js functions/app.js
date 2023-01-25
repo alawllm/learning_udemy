8c5432da8dff65da73dd45e5fb5cd3bb8aec8403
alawllm <alicja.willam@gmail.com>
2023-01-25 21:26:54 +0100

    Spread in object literals, Rest params

--- javascript/newer js functions/app.js
+++ javascript/newer js functions/app.js
@@ -34,7 +34,63 @@
 
 // Spread with array literals
 
-const cats = ['Blue', 'Scout', 'Rocket'];
-const dogs = ['Rusty', 'Wyatt'];
+// const cats = ['Blue', 'Scout', 'Rocket'];
+// const dogs = ['Rusty', 'Wyatt'];
 
-const allPets = [...cats, ...dogs]
+// const allPets = [...cats, ...dogs]
+
+
+// Spread in object literals
+// - used to copy properties from one object
+// into another object
+// 
+// const feline = { legs: 4, family: 'Felidae' };
+// const canine = { isFurry: true, family: 'Caninae' }
+
+// if 2 after another the last one wins
+
+// {...[2,3,4,5,6]}
+// {0: 2, 1: 3, 2: 4, 3: 5, 4: 6}
+// {...'HIIII'}
+// {0: 'H', 1: 'I', 2: 'I', 3: 'I', 4: 'I'}
+
+// const dataFromForm = {
+//     email: 'blabla@gmail.com',
+//     passwoed: 'bebebe',
+//     username: 'tfunke'
+// }
+
+// const newUser = { ...dataFromForm, id: 2345, isAdmin: false }
+
+// newUser
+// {email: 'blabla@gmail.com', passwoed: 'bebebe', username: 'tfunke', id: 2345, isAdmin: false}
+
+// Rest Params
+
+// Arguments
+
+// function sum() {
+//     console.log(arguments)
+// }
+
+
+// this doesnt work!! arguments is not an array
+// function sum() {
+//     return arguments.reduce((total, el) => total + el)
+// }
+
+// REST - collects all remaining arguments and puts them
+// into an array
+
+// function sum(...nums) {
+//     return nums.reduce((total, el) => total + el)
+// }
+
+// ... collects an array 
+// collecting things in into an array 
+
+// function raceResults(gold, silver, ...everyoneElse) {
+//     console.log(`Gold medal goes to: ${gold}`)
+//     console.log(`Silver medal goes to: ${silver}`)
+//     console.log(`And thanks to everyone else ${everyoneElse}`)
+// }
---
+++ logical operators/app.js
@@ -0,0 +1,13 @@
+// const password = prompt("Enter your password")
+// if (password.length >= 6 && password.indexOf(" ") === -1) { console.log("Valid password") }
+// else { console.log("Incorrect") }
+
+// const age = 5
+// if (age >= 0 && age < 5 || age >= 65) { console.log("free") }
+// else if (age >= 5 && age < 10) {
+//     console.log("10$")
+// } else if (age >= 10 && age < 65) { console.log("40$") }
+// else { console.log("invalid age") }
+
+const firstName = prompt("enter your first name");
+if (!firstName) { console.log("False") }
