32e27a37ea3c0a6ecea52b7786365fc73d9b8565
alawllm <alicja.willam@gmail.com>
2023-02-09 19:58:10 +0100

    random background color generator ready

--- DOM_Events/RandomColors/app.js
+++ DOM_Events/RandomColors/app.js
@@ -1,9 +1,15 @@
 // Random color generator 
 
+// random color generator 
+
 const btn = document.querySelector('.btn')
 
 function randomColorGenerator() {
-    return console.log('it works')
+    let c1 = Math.floor(100 + Math.random() * 100)
+    let c2 = Math.floor(100 + Math.random() * 100)
+    let c3 = Math.floor(100 + Math.random() * 100)
+    let colors = `rgb(${c1},${c2},${c3})`
+    return colors
 }
 
 function changeColor() {
