43878b35dd9e04a12e0c931a9866d427d70dcb15
alawllm <alicja.willam@gmail.com>
2023-02-10 20:54:46 +0100

    event object

---
+++ DOM_Events/TheEventObject/app.js
@@ -0,0 +1,34 @@
+// event object - automatically passed in
+// into the function 
+// object created automatically 
+// contains information about the event 
+
+document.querySelector('button').addEventListener('click', function(evt) {
+    console.log(evt)
+})
+
+// const input = document.querySelector('input');
+// input.addEventListener('keydown', function(e){
+//     console.log(e.key)
+//     console.log(e.code)
+
+// })
+
+// input.addEventListener('keyup', function(){
+//     console.log('KEYUP')
+// })
+
+// if looking for an event in the whole window  
+
+window.addEventListener('keydown', function(e){
+    switch(e.code){
+        case'ArrowUp':
+        console.log('UP!')
+        break;
+        case'ArrowDown':
+        console.log('Arrow Down!')
+        break;
+        default: 
+        console.log('IGNORED')
+    }
+})
