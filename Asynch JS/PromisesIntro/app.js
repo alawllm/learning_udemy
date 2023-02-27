f098be5a7856639a561733b9a400012c25e0e4c0
alawllm <alicja.willam@gmail.com>
2023-02-27 16:44:44 +0100

    promise concept

---
+++ Asynch JS/PromisesIntro/app.js
@@ -0,0 +1,81 @@
+// THE CALLBACK VERSION
+// passing to callbacks - success or failure 
+
+const fakeRequestCallback = (url, success, failure) => {
+    const delay = Math.floor(Math.random() * 4000) + 500;
+    setTimeout(() => {
+        if (delay > 4000) {
+            failure('Connection Timeout :(')
+        } else {
+            success(`Here is your fake data from ${url}`)
+        }
+    }, delay)
+}
+// THE PROMISE VERSION 
+// expects only url, not the success and failure 
+
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+// response passed to success,err to failure
+// CALLBACK HELL 
+
+// fakeRequestCallback('books.com/page1',
+//     function (response) {
+//         console.log('it worked!')
+//         console.log(response)
+//         fakeRequestCallback('books.com/page2',
+//             function (response) {
+//                 console.log('it worked again!')
+//                 console.log(response)
+//                 fakeRequestCallback('books.com/page3',
+//                     function (response) {
+//                         console.log('it worked again!')
+//                         console.log(response)
+//                     }, function (err) {
+//                         console.log('ERROR(3nd req!)', err)
+//                     })
+//             },
+//             function (err) {
+//                 console.log('ERROR(2nd req!)', err)
+//             })
+//     }, function (err) {
+//         console.log('it didnt work', err)
+
+//     })
+
+
+// also kind of nesting 
+// .then and .catch methods 
+
+fakeRequestPromise('yelp.com/api/coffee/page1')
+    .then(() => {
+        console.log('it worked! (page 1)')
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+            .then(() => {
+                console.log('promise resolved (page 2)')
+                fakeRequestPromise('yelp.com/api/coffee/page3')
+                    .then(() => {
+                        console.log('promise resolved (page 3)')
+                    })
+                    .catch(() => {
+                        console.log(('rejected (page 3)'))
+                    })
+            })
+
+            .catch(() => {
+                console.log(('rejected (page 2)'))
+            })
+    }).catch(() => {
+        console.log(('rejected (page 1)'))
+    }) 
